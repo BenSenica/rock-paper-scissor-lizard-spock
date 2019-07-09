0b8ec576a5edd11cd29c61885276f7c91b2c6ea3
Ben Senica <senicaben@gmail.com>
2019-07-09 14:33:06 +0200

    expanded game to include lizard and spock options

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let players = document.getElementById("players");
 players.addEventListener("change", changeGame);
 let p1choice = document.getElementById("p1choice");
 let p2choice = document.getElementById("p2choice");
-let choices = ["rock", "paper", "scissors"];
+let choices = ["rock", "paper", "scissors", "lizard", "spock"];
 for (choice of choices) {
     let element = document.createElement("option");
     element.value = choice;
@@ -35,9 +35,14 @@ function playGame(event) {
         } else if (p2value === "paper") {
             result = "Paper covers Rock!" + " <br>" + "Player 2 wins!";
             document.getElementById("result").innerHTML = result;
-        } else {
+        } else if (p2value === "scissors") {
             result = "Rock crushes Scissors" + " <br>" + "Player 1 wins!";
             document.getElementById("result").innerHTML = result;
+        } else if (p2value === "lizard") {
+            result = "Rock crushes Lizard!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else {
+            result = "Spock vaporizes Rock!" + " <br>" + "Player 2 wins!";
         }
     } else if (p1value === "paper") {
         if (p2value === "rock") {
@@ -46,16 +51,62 @@ function playGame(event) {
         } else if (p2value === "paper") {
             result = "Draw!";
             document.getElementById("result").innerHTML = result;
-        } else {
+        } else if (p2value === "scissors") {
             result = "Scissors cuts Paper!" + " <br>" + "Player 2 wins!";
             document.getElementById("result").innerHTML = result;
+        } else if (p2value === "lizard") {
+            result = "Lizard eats Paper!" + " <br>" + "Player 2 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else {
+            result = "Paper disproves Spock!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
         }
-    } else {
+    } else if (p1value === "scissors") {
         if (p2value === "rock") {
             result = "Rock crushes Scissors!" + " <br>" + "Player 2 wins!";
             document.getElementById("result").innerHTML = result;
         } else if (p2value === "paper") {
             result = "Scissors cuts Paper!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "scissors") {
+            result = "Draw!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "lizard") {
+            result = "Scissors decapitates Lizard!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else {
+            result = "Spock smashes Scissors!" + " <br>" + "Player 2 wins!";
+            document.getElementById("result").innerHTML = result;
+        }
+    } else if (p1value === "lizard") {
+        if (p2value === "rock") {
+            result = "Rock crushes Lizard!" + " <br>" + "Player 2 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "paper") {
+            result = "Lizard eats Paper!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "scissors") {
+            result = "Scissors decapitates Lizard!" + " <br>" + "Player 2 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "lizard") {
+            result = "Draw!";
+            document.getElementById("result").innerHTML = result;
+        } else {
+            result = "Lizard poisons Spock!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        }
+    } else {
+        if (p2value === "rock") {
+            result = "Spock vaporizes Rock!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "paper") {
+            result = "Paper disproves Spock!" + " <br>" + "Player 2 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "scissors") {
+            result = "Spock smashes Scissors!" + " <br>" + "Player 1 wins!";
+            document.getElementById("result").innerHTML = result;
+        } else if (p2value === "lizard") {
+            result = "Lizard poisons Spock!" + " <br>" + "Player 2 wins!";
             document.getElementById("result").innerHTML = result;
         } else {
             result = "Draw!";
